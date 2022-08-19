8bcd87248d6592b28247804a92aa8b2b51c5ef37
orionEphraim <109883121+orionEphraim@users.noreply.github.com>
2022-08-19 23:41:25 +0000

    Add files via upload

---
+++ Orion.py
@@ -0,0 +1,37 @@
+import random
+
+
+def guess(x):
+    random_number = random.randint(1, x)
+    guess = 0
+    while guess != random_number:
+        guess = int(input(f"Guess a number between 1 and {x}: "))
+        if guess < random_number:
+            print('Sorry, guess again. Too low. ')
+        elif guess > random_number:
+            print('Sorry, guess too high. ')
+
+    print(f'Bravo, congrats. You have guessed the correct number {random_number} !!')
+
+
+guess(10)
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback!= 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low # could also be high b/c low = high
+        guess = random.randint(low, high)
+        feedback = input(f' Is {guess} too high (H), too low (L), or correctly(C)')
+        if feedback == 'l':
+            low = guess + 1
+
+    print(f"Congrats you have guessed the number {guess},correctly!!!")
+
+
+
+
